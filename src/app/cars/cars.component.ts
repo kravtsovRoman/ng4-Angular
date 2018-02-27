594f986ab141d40fb94fb5e1dcad862cd91874ed
Kravtsov Roman <rozario91208@gmail.com>
2018-02-27 18:13:50 +0200

    test

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent {
+
+  addCarStatus = '';
+  inputText = '';
+
+  constructor() {
+  }
+
+  addCar() {
+    this.addCarStatus = 'Car add...';
+  }
+
+  onKeyUp(event) {
+    this.inputText = event.target.value;
+  }
+}

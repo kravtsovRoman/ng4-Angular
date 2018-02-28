d8b6a451f135d79986d06a29bd93ecf012e497d0
kravtsovRoman <rozario91208@gmail.com>
2018-02-28 13:01:59 +0200

    *ngFor, [ngStyle], [ngClass]

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,11 +9,18 @@ export class CarsComponent {
 
   carName = '';
   addCarStatus = false;
-
+  cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
+ 
   constructor() {
   }
 
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
+  }
+
+  setBigCarText(car: string) {
+    return car.length > 4 ? true : false;
   }
 }
